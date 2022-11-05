63ecb5beec1fedd99cb501d60bb814ff8facd080
yassine ben azouz <yassinebenazouz123@gmail.com>
2022-11-05 19:13:27 +0200

    finished navbar animation

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 import $ from "jquery";
+function smoothScroll(element, duration) {
+    $("html, body").animate(
+        {
+            scrollTop: $(element.hash).offset().top,
+        },
+        duration)
+}
+$("#about-me-link").on("click", function (event) {
+    event.preventDefault();
+    smoothScroll(this, 800)
+});
+$("#skills-link").on("click", function (event) {
+    event.preventDefault();
+    smoothScroll(this, 1000)
+});
+$("#project-link").on("click", function (event) {
+    event.preventDefault();
+    smoothScroll(this, 1200)
+});
+$("#contact-link").on("click", function (event) {
+    event.preventDefault();
+    smoothScroll(this, 1400)
+});
+/////////////////////
+let reference = document.querySelector(".reference")
+let nav = document.querySelector("nav")
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            nav.className = "nav-normal-mode"
+        } else {
+            nav.className = "nav-scrolling-mode"
+        }
+    })
+})
+observer.observe(reference)
